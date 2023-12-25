dd7f6574062e406a5050f4944e5b0ad2e0b80472
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-12-25 15:04:09 +0200

    Update 03 Students Table.py

--- pages/03 Students Table.py
+++ pages/03 Students Table.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="GSB", page_icon="16.png")
 st.image("GSB Medium.png", width=120,)
 
-st.title("GSB Results System")
+st.title("GSB Students Table")
 components.iframe("https://app.powerbi.com/view?r=eyJrIjoiMzU2NDQ1N2QtYjNjMy00Yzg5LWIyNzMtM2FlYzNkMzA2NGRlIiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9", width=800, height=600)
 
 hide_st_style = """
